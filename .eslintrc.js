dec15b2bc47807087011a1beebf7b190323ec59e
tgmeng <tgmeng@163.com>
2019-09-19 09:16:41 +0800

    config: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,21 @@ module.exports = {
   extends: ['eslint:recommended', 'airbnb-base'],
   plugins: ['jest'],
   env: {
-    'jest/globals': true
+    'jest/globals': true,
   },
   rules: {
     'max-classes-per-file': 0,
-    'no-plusplus': 0
-  }
+    'no-plusplus': 0,
+    'no-underscore-dangle': 0,
+    'no-param-reassign': 0,
+    'no-await-in-loop': 0,
+    'import/prefer-default-export': 0,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
 };
