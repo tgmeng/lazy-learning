ca04ae968e55af8d3d26bf7cc3401360e5c35aaa
tgmeng <tgmeng@163.com>
2020-10-15 20:24:26 +0800

    hefactor: bubble-sort & jestrunner config

--- packages/algorithms/sort/bubble-sort.ts
+++ packages/algorithms/sort/bubble-sort.ts
@@ -22,25 +22,7 @@ export function bubbleSortP(arr) {
         swapped = true;
       }
     }
-    if (swapped) {
-      return arr;
-    }
-  }
-  return arr;
-}
-
-export function bubbleSortPP(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    let swapped = false;
-    for (let j = 0; j < arr.length - 1 - i; j++) {
-      if (arr[j] > arr[j + 1]) {
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
-        swapped = true;
-      }
-    }
-    if (swapped) {
+    if (!swapped) {
       return arr;
     }
   }
